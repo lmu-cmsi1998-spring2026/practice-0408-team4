2cb09477d88510dea2b436c2482ed84bed13ac3b
cole <coleschulein@gmail.com>
2026-04-06 18:44:39 -0700

    Established days_in_month

---
+++ src/days_in_month.py
@@ -0,0 +1,11 @@
+from is_leap_year import is_leap_year
+
+def days_in_month(year: int, month: int) -> int:
+    if month == 2:
+        return 29 if is_leap_year(year) else 28
+    elif month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    elif month in [4, 6, 9, 11]:
+        return 30
+    else:
+        return None
